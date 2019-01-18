b15f9caaa2057ed050ae32a970d201c686119f03
Paul Campbell (CMD) <paulcam@microsoft.com>
2019-01-17 16:57:39 -0800

    Rework BaseElement

--- source/shared/cpp/AdaptiveCardsSharedModel/AdaptiveCardsSharedModelUnitTest/EverythingBagel.cpp
+++ source/shared/cpp/AdaptiveCardsSharedModel/AdaptiveCardsSharedModelUnitTest/EverythingBagel.cpp
@@ -476,8 +476,10 @@ namespace AdaptiveCardsSharedModelUnitTest
             // re-serialize the card and verify
 
             // uncomment the following line to output the serialized json
-            //Logger::WriteMessage("Submit Data: '"s.append(everythingBagel->Serialize()).append("'").c_str());
-            Assert::AreEqual(std::string(EVERYTHING_JSON), everythingBagel->Serialize());
+            Logger::WriteMessage("Submit Data: '"s.append(everythingBagel->Serialize()).append("'").c_str());
+            const std::string expectedJson {EVERYTHING_JSON};
+            const std::string actualJson {everythingBagel->Serialize()};
+            Assert::AreEqual(expectedJson, actualJson);
         }
     };
 }
